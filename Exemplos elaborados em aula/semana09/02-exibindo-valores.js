d34100b77e0855a7ca4dc4e72bd9f8f3604d15e1
Leonardo Villani <leonardo.villani@fatec.sp.gov.br>
2020-05-18 14:37:50 -0300

    Exemplos elaborados na semana 09

---
+++ Exemplos elaborados em aula/semana09/02-exibindo-valores.js
@@ -0,0 +1,8 @@
+var clubes = ['Palmeiras', 'Santos', 'Corinthians', 'São Paulo'];
+
+// toString() - Apresenta os valores do vetor separados por vírgula.
+alert(clubes);
+alert(clubes.toString());
+
+// join(separador) - Apresentar os valores do vetor separados pelo 'separador'
+alert(clubes.join(' - '));
---
+++ Exemplos elaborados em aula/semana09/03-inserindo-elementos.js
@@ -0,0 +1,12 @@
+// Definindo um vetor com um elemento.
+var religiao = ['católica'];
+
+// Inserindo o segundo elemento no final do vetor.
+// push(valor) - empurrar - elemento inserido no final da fila. 
+religiao.push('evangélica');
+
+// Inserindo o terceiro elemento no início do vetor.
+// unshift(valor) - deslocando - os elementos anteriores de posição - inseri no ínicio
+religiao.unshift('islâmica');
+
+console.log(religiao);
